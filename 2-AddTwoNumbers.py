fe1ceaca0ec26b23c36ad9382d4690792670f5a2
M-Awwab-Khan <mawwabkhank2006@gmail.com>
2024-04-09 23:24:17 +0500

    add corresponding numbers of linked list

---
+++ 2-AddTwoNumbers.py
@@ -0,0 +1,26 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        dummy_head = ListNode()
+        current = dummy_head
+        carry = 0
+
+        while l1 or l2 or carry:
+            sum_val = carry
+            if l1:
+                sum_val += l1.val
+                l1 = l1.next
+            if l2:
+                sum_val += l2.val
+                l2 = l2.next
+            
+            carry, digit = divmod(sum_val, 10)
+            current.next = ListNode(digit)
+            current = current.next
+
+        return dummy_head.next
+            
