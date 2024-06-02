5cb7eb41933f1d65e5b367f8fce9b7a81225b573
M-Awwab-Khan <mawwabkhank2006@gmail.com>
2024-06-02 11:46:41 +0500

    33 solved

---
+++ 33-SearchinRotatedArray.py
@@ -0,0 +1,25 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        begin = 0
+        end = len(nums) - 1 
+        while begin <= end:
+            mid = (begin + end)//2
+            if nums[mid] == target:
+                return mid
+            if nums[begin] == target:
+                return begin
+            if nums[end] == target:
+                return end
+            if nums[mid] == nums[end]:
+                end -= 1 
+            elif nums[mid] > nums[end]: 
+                if  nums[begin] <= target and target < nums[mid]: 
+                    end = mid - 1
+                else:
+                    begin = mid + 1
+            else:
+                if  nums[mid] < target and target <= nums[end]: 
+                    begin = mid + 1
+                else:
+                    end = mid - 1
+        return -1
