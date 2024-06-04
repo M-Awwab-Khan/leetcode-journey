174c9ad6d5505cf6b8f70ccc6ddc8afb67315288
M-Awwab-Khan <mawwabkhank2006@gmail.com>
2024-06-04 12:57:47 +0500

    what the heck?

---
+++ 540-SingleElementInASortedArray.py
@@ -0,0 +1,16 @@
+class Solution:
+    def singleNonDuplicate(self, nums: List[int]) -> int:
+        left, right = 0, len(nums) - 1
+        while left < right:
+            mid = (left + right) // 2
+            if mid % 2 == 0:
+                if nums[mid] != nums[mid+1]:
+                    right = mid
+                else:
+                    left = mid + 1
+            else:
+                if nums[mid] != nums[mid-1]:
+                    right = mid - 1
+                else:
+                    left = mid + 1
+        return nums[left]
