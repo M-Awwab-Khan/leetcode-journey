ade1de682880a7155a2f7b43359ec82f9c997204
M-Awwab-Khan <mawwabkhank2006@gmail.com>
2024-06-15 12:52:23 +0500

    ez pz cpp solution

---
+++ 1051-HeightChecker.cpp
@@ -0,0 +1,16 @@
+#include <vector>
+
+class Solution {
+public:
+    int heightChecker(vector<int>& heights) {
+        int count = 0;
+        vector<int> expected = heights;
+        sort(expected.begin(), expected.end());
+        for (int i = 0; i < heights.size(); i++) {
+            if (heights[i] != expected[i]) {
+                count++;
+            }
+        }
+        return count;
+    }
+};
